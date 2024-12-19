7df49269c36b18e7c16b77e081b694f26825b786
orb <798008773@qq.com>
2024-12-20 00:26:07 +0800

    feat: 矩阵变换

--- lib.js
+++ lib.js
@@ -26,3 +26,74 @@ function initShader(gl, vertexShaderSource, fragmentShaderSource) {
 
   return program;
 }
+
+
+/**
+ * 旋转矩阵
+ * @param {number} deg
+ * @returns {Float32Array}
+ */
+const getRotateMatrix = (deg) => {
+  const cos = Math.cos(deg)
+  const sin = Math.sin(deg)
+
+  return new Float32Array([
+      cos,  sin, 0, 0,
+     -sin,  cos, 0, 0,
+        0,    0, 1, 0,
+        0,    0, 0, 1
+  ])
+}
+
+/**
+ * 缩放矩阵
+ * @param {number} x=1
+ * @param {number} y=1
+ * @param {number} z=1
+ * @returns {Float32Array}
+ */
+const getScaleMatrix = (x = 1, y = 1, z = 1) => {
+  return new Float32Array([
+    x, 0, 0, 0,
+    0, y, 0, 0,
+    0, 0, z, 0,
+    0, 0, 0, 1
+  ])
+}
+
+/**
+ * 平移矩阵
+ * @param {number} x=0
+ * @param {number} y=0
+ * @param {number} z=0
+ * @returns {Float32Array}
+ */
+const getTranslateMatrix = (x = 0, y = 0, z = 0) => {
+  return new Float32Array([
+    1, 0, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 1, 0,
+    x, y, z, 1
+  ])
+}
+
+
+/**
+ * 矩阵复合
+ * @param {Float32Array} m1
+ * @param {Float32Array} m2
+ * @returns {Float32Array}
+ */
+const multiplyMatrix = (m1, m2) => {
+  const result = new Float32Array(16)
+  
+  for (let i = 0; i < 4; i++) {
+    result[i] = m1[i] * m2[0] + m1[i + 4] * m2[1] + m1[i + 8] * m2[2] + m1[i + 12] * m2[3]
+    result[i + 4] = m1[i] * m2[4] + m1[i + 4] * m2[5] + m1[i + 8] * m2[6] + m1[i + 12] * m2[7]
+    result[i + 8] = m1[i] * m2[8] + m1[i + 4] * m2[9] + m1[i + 8] * m2[10] + m1[i + 12] * m2[11]
+    result[i + 12] = m1[i] * m2[12] + m1[i + 4] * m2[13] + m1[i + 8] * m2[14] + m1[i + 12] * m2[15]
+  }
+
+  return result
+}
+
